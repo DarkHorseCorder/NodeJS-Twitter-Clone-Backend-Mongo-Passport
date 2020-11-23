d3fd412da531693e35699caf9c8052c030820a02
DarkHorseCoder <120799088+DarkHorseCorder@users.noreply.github.com>
2020-11-24 06:23:04 +0900

    serializers

---
+++ serializers/post.serializer.js
@@ -0,0 +1,55 @@
+const mongoose = require("mongoose");
+const { serializeUser } = require("./user.serializer");
+
+exports.serializePost = async (post, client, level = 0) => {
+  if (level > 1) {
+    return;
+  }
+  if (!post) return;
+  if (!post instanceof mongoose.Document) throw Error("Unknown post type");
+  post = await post
+    .populate("user")
+    .populate("retweeted_status")
+    .populate("quoted_status")
+    .execPopulate();
+
+  //serialize embedded posts
+  let retweeted_status = await this.serializePost(
+    post.retweeted_status,
+    client,
+    level + 1
+  );
+  let quoted_status = await this.serializePost(
+    post.quoted_status,
+    client,
+    level + 1
+  );
+
+  //serialize user field
+  if (!post.user) throw Error("Post doesnt have a user field");
+  let user = await serializeUser(post.user, client);
+
+  post = post.toObject();
+  //serialize post if necessary
+  let favorited = await mongoose
+    .model("Friendship")
+    .isLiked(client && client._id, post._id);
+  let retweeted = await mongoose
+    .model("Friendship")
+    .isReposted(client && client._id, post._id);
+  return {
+    ...post,
+    favorited,
+    retweeted,
+    user,
+    retweeted_status,
+    quoted_status,
+  };
+};
+
+exports.serializePosts = async (posts = [], client) => {
+  if (!posts instanceof Array)
+    //includes CoreDocumentArray
+    throw Error("Unknown type");
+  return Promise.all(posts.map((post) => this.serializePost(post, client)));
+};
---
+++ serializers/user.serializer.js
@@ -0,0 +1,29 @@
+const mongoose = require("mongoose");
+
+/**
+ * serializes user with fields required by client user
+ */
+exports.serializeUser = async (user, client = null) => {
+  if (!user) return;
+
+  if (user.toObject) user = user.toObject();
+  let followers_count = await mongoose
+    .model("Friendship")
+    .countFollowers(user._id);
+  let friends_count = await mongoose.model("Friendship").countFriends(user._id);
+  let statuses_count = await mongoose.model("User").countPosts(user._id);
+  let following = await mongoose
+    .model("Friendship")
+    .isFollowing(client && client._id, user._id);
+  return {
+    ...user,
+    following,
+    followers_count,
+    friends_count,
+    statuses_count,
+  };
+};
+exports.serializeUsers = async (users = [], client) => {
+  if (!users instanceof Array) throw Error("Unknown type");
+  return Promise.all(users.map((user) => this.serializeUser(user, client)));
+};
